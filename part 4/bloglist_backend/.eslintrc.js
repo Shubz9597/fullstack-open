d68294944d0ed6bdeafae8ece8a931d94668ffdd
Shubz9597 <shubham.misra95@gmail.com>
2021-08-28 05:47:34 +0530

    Exercise 4.2 Complete, Barely left by the Murphy's Law

---
+++ part 4/bloglist_backend/.eslintrc.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'eqeqeq' : 'error',
+		'no-trailing-spaces': 'error',
+    	'object-curly-spacing': [
+        	'error', 'always'
+    	],
+    	'arrow-spacing': [
+        	'error', { 'before': true, 'after': true }
+    	// eslint-disable-next-line no-mixed-spaces-and-tabs
+    	],
+		'no-console' : 0
+	}
+}
