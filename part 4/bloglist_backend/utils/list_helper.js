b3bd044890117d1b211ae46c0c2337d81a43b1d6
Shubz9597 <shubham.misra95@gmail.com>
2021-08-30 03:13:43 +0530

    Exercise 4.4 done

--- part 4/bloglist_backend/utils/list_helper.js
+++ part 4/bloglist_backend/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
+	console.log(blogs)
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	return blogs.length === 0 ? 0 : blogs.reduce((sum, order) => sum + order.likes, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
