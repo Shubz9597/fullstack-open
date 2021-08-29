14bb3fd6a7039017219273e7c69d8254a673d963
Shubz9597 <shubham.misra95@gmail.com>
2021-08-30 04:13:52 +0530

    Made changes to the test suite files, and Exercise 4.5 completed

--- part 4/bloglist_backend/tests/dummy.test.js
+++
@@ -1,7 +0,0 @@
-const listHelper = require('../utils/list_helper').dummy
-
-test('dummy return 1', () => {
-	const blogs = []
-
-	expect(listHelper(blogs)).toBe(1)
-})
--- part 4/bloglist_backend/utils/list_helper.js
+++ part 4/bloglist_backend/utils/list_helper.js
@@ -7,7 +7,16 @@ const totalLikes = (blogs) => {
 	return blogs.length === 0 ? 0 : blogs.reduce((sum, order) => sum + order.likes, 0)
 }
 
+
+const favoriteBlog = (blogs) => {
+	return blogs.length === 0 ? {} :
+		blogs.reduce((max, order) => {
+			return order.likes > max.likes ?  order : max
+		}, { likes : Number.MIN_SAFE_INTEGER })
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
