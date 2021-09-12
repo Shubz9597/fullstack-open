96200bbaaf24da8e4b2429b8fe6dac8843388ae6
Shubz9597 <shubham.misra95@gmail.com>
2021-09-13 00:13:55 +0530

    Updated the mongoose connect options according to old mongoose version

--- part 4/bloglist_backend/app.js
+++ part 4/bloglist_backend/app.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 
 logger.info('Connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 	.then(() => {
 		logger.info('Connected to MongoDB')
 	})
