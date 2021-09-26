d80085332afaecde3874f7178a7a209e3b088814
Shubz9597 <shubham.misra95@gmail.com>
2021-09-27 02:42:40 +0530

    Added the functionality that only logged in users can enter the blog

--- part 4/bloglist_backend/controllers/blogs.js
+++ part 4/bloglist_backend/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog
@@ -11,6 +21,14 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
+	const token = getTokenFrom(request)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+
+	if(!token || !decodedToken.id) {
+		return response.status(401).send({
+			error: 'token missing or invalid'
+		})
+	}
 
 	const user = await User.findById(body.userId)
 
