c00beb1b911b5dbe9a3f8807550f5c6d978d318c
Shubz9597 <shubham.misra95@gmail.com>
2021-09-28 03:32:21 +0530

    Added tokenExtractor middleware and completed exercise 4.20

--- part 4/bloglist_backend/controllers/blogs.js
+++ part 4/bloglist_backend/controllers/blogs.js
@@ -4,14 +4,6 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization')
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.substring(7)
-	}
-	return null
-}
-
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog
 		.find({}).populate('user' ,{ username: 1, name: 1 })
@@ -21,10 +13,9 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
-	const token = getTokenFrom(request)
-	const decodedToken = jwt.verify(token, process.env.SECRET)
+	const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-	if(!token || !decodedToken.id) {
+	if(!request.token || !decodedToken.id) {
 		return response.status(401).send({
 			error: 'token missing or invalid'
 		})
